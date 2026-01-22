8664ca71c2723d86cd1b3615124a12c593d675bb
pope-h <hamatthan@gmail.com>
2026-01-22 20:03:42 +0100

    updated app module

--- BackEnd/src/app.module.ts
+++ BackEnd/src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SubmissionsModule } from './modules/submissions/submissions.module';
+import { StellarModule } from './modules/stellar/stellar.module';
+import { NotificationsModule } from './modules/notifications/notifications.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: process.env.NODE_ENV === 'development', // Don't use in production
+    }),
+    AuthModule,
+    SubmissionsModule,
+    StellarModule,
+    NotificationsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
